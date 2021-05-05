36c8575388db7c0ebc918180c9282167533d82d2
khinmarlaroo <khinmarlar19793@gmail.com>
2021-05-05 10:40:59 +0630

    change project structure

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,48 +1,44 @@
 class ArticlesController < ApplicationController
-  def index
-    @articles = Article.all
-  end
-
-  def show
-    @article = Article.find(params[:id])
-  end
-
-  def new
-    @article = Article.new
-  end
-
-  def create
-    @article = Article.new(article_params)
-
-    if @article.save
-      redirect_to @article
-    else
-      render :new
+    def index
+      @articles = ArticlesService.listAll
     end
-  end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
-
-  def update
-    @article = Article.find(params[:id])
-
-    if @article.update(article_params)
-      redirect_to @article
-    else
-      render :edit
+    def show
+      @article = ArticlesService.findArticleById(params[:id])
     end
-  end
 
-  def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
+    def new
+      @article = ArticlesService.newArticle
+    end
 
-    redirect_to root_path
-  end
+    def create
+      isSaveArticle = ArticlesService.createArticle(article_params)
+        if isSaveArticle
+          redirect_to articles_path
+        else
+          render :new
+        end
+    end
 
-  private
+    def edit
+      @article = ArticlesService.findArticleById(params[:id])
+    end
+
+    def update
+      isUpdateArticle = ArticlesService.updateArticle(params[:id],article_params)
+      if isUpdateArticle
+          redirect_to articles_path
+      else
+          render :edit
+      end
+    end
+
+    def destroy
+        ArticlesService.deleteArticle(params[:id])
+        redirect_to root_path
+    end
+
+    private
     def article_params
       params.require(:article).permit(:title, :body)
     end
